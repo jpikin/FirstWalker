3d04665693ad924c79b4f2e52fba7ede3ac047a8
pikin <pikin@bk.ru>
2024-03-02 16:56:08 +0500

    setting tempButton added

--- src/main/java/Program/WalkerSettings.java
+++ src/main/java/Program/WalkerSettings.java
@@ -1,14 +1,25 @@
 package Program;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class WalkerSettings extends JFrame {
     private static final int WINDOW_HEIGHT = 450;
     private static final int WINDOW_WIDTH = 720;
+    JButton tempButton = new JButton("Settings в разработке, Нажмите для выхода");
 
     WalkerSettings(){
         setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
         setTitle("Settings");
         setVisible(false);
+
+        add(tempButton);
+        tempButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setVisible(false);
+            }
+        });
     }
 }
